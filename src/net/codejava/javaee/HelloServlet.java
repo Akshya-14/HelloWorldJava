17816b5ce9ce50417806e743657b06516504effc
ezkumak <Akshya.Kumar.Jena@ericsson.com>
2021-08-28 20:01:52 +0530

    Uploading to make build success.

--- src/net/codejava/javaee/HelloServlet.java
+++ src/net/codejava/javaee/HelloServlet.java
@@ -30,7 +30,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		String yourName = request.getParameter("yourName");
-		//String hobby = request.getParameter("hobby");
+		String hobby = request.getParameter("hobby");
 		PrintWriter writer = response.getWriter();
 		writer.println("<h1>Hello " + yourName + "</h1>");
 		writer.println("<h1>Your hobby is : " + hobby + "</h1>");
